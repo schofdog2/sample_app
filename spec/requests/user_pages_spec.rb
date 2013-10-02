a20b41d16ce721c52b2d9c4e7f83c0f639e00136
Chris <chris.symons96@gmail.com>
2013-10-02 11:43:27 -0700

    Add sign up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,6 @@ describe "UserPages" do
 
   describe "signup page" do
     before { visit signup_path }
-
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
